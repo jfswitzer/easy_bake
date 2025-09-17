c97aeb747de57a88b9d4a4979e7ba30a628c7029
Jennifer Switzer <jfi.switzer@gmail.com>
2025-09-16 20:09:33 -0700

    heartbeat service

---
+++ testing/heartbeat/heartbeat.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Set log file path
+LOGFILE="$HOME/temperature_log.csv"
+
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+
+TEMP=$(vcgencmd measure_temp core)
+VOLTAGE=$(vcgencmd measure_volts arm)
+FREQUENCY_CORE=$(vcgencmd measure_clock core)
+FREQUENCY_ARM=$(vcgencmd measure_clock arm)
+
+echo "$TIMESTAMP,$TEMP,$VOLTAGE,$FREQUENCY_CORE,$FREQUENCY_ARM" >> "$LOGFILE"
