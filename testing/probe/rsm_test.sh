6f1c7d08625b701e1bddc673d6ec633124ab817c
Jennifer Switzer <jfi.switzer@gmail.com>
2026-03-12 02:28:05 -0700

    loop

--- testing/probe/rsm_test.sh
+++ testing/probe/rsm_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-WORKING_DIR="/home/baking/easy_bake/testing/probe"
+WORKING_DIR="/home/jen/ASPLOS_27/easy_bake/testing/probe"
 DATETIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 VOLTAGE=$(vcgencmd measure_volts core)
 echo $DATETIME
 echo $VOLTAGE
 mkdir -p ${WORKING_DIR}/logs
 for i in $(seq 1 $2); do
+    echo $i
     (time stress-ng --cpu 4 --log-file log.log --cpu-method $1 --verify --abort -v -t 1m) 2>&1 | tee -a ${WORKING_DIR}/logs/${DATETIME}_${VOLTAGE}_$1_$2min_eb_probe.log
 done
